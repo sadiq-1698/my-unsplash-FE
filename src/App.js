5a570591961d75d9b8d9819b53890fc0a7e71d7b
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-11 21:33:39 +0530

    testing button variants

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <h1>My unsplash</h1>;
+  return (
+    <div>
+      <Button
+        color="red"
+        text="Add Image"
+        onClick={() => console.log("Added Image!")}
+      />
+      <Button
+        color="red"
+        text="Delete Image"
+        onClick={() => console.log("Deleted image!")}
+      />
+      <Button text="Primary" onClick={() => console.log("Primary Image!")} />
+    </div>
+  );
 }
 
 export default App;
