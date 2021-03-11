c64ead5a3d19f4944e4fdd34a1f6ff438e5ce9a5
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-11 21:42:26 +0530

    global styles and tests

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
-import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div>
-      <Button
-        color="red"
-        text="Add Image"
-        onClick={() => console.log("Added Image!")}
-      />
-      <Button
-        color="red"
-        text="Delete Image"
-        onClick={() => console.log("Deleted image!")}
-      />
-      <Button text="Primary" onClick={() => console.log("Primary Image!")} />
+      <p>MY UNSPLASH</p>
     </div>
   );
 }
