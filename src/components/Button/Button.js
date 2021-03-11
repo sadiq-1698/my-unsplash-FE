1a6d2debc8d971a2737228c0b2b56e1ac1d502df
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-11 21:34:08 +0530

    Added props in component definition

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,21 @@
 import "./styles.css";
+import PropTypes from "prop-types";
 
-const Button = () => {
-  return <button>Button</button>;
+const Button = ({ color, text, ...props }) => {
+  return (
+    <button className={`color-${color} button`} {...props}>
+      {text}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string.isRequired
+};
+
+Button.defaultProps = {
+  color: ""
 };
 
 export default Button;
