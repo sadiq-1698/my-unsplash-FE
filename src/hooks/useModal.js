ef5a83cfd8a12323f90c84879b1e5894253385f9
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-14 00:33:44 +0530

    added custom hook for modal states

---
+++ src/hooks/useModal.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const useModal = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => setModalOpen(prev => !prev);
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  const modalState = {
+    modalOpen,
+    setModalOpen
+  };
+
+  return {
+    toggleModal,
+    openModal,
+    closeModal,
+    modalOpen,
+    modalState
+  };
+};
+
+export default useModal;
