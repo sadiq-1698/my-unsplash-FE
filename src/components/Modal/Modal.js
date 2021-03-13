c9a5e03e93c6cad0bd1b5a3835a1d38987980140
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-14 00:34:38 +0530

    added barrier dismissal on click property on the modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,22 +1,43 @@
 import { Fragment } from "react";
 import PropTypes from "prop-types";
+
 import "./styles.css";
 
-const Modal = ({ children, isOpen }) => {
+const Modal = ({ children, modalState, barrierDismiss, shouldCloseOnEsc }) => {
   return (
     <Fragment>
-      {isOpen && (
-        <div className="modal-backdrop">
-          <div className="modal-box">{children}</div>
+      {modalState.modalOpen && (
+        <div className="modal-backdrop" onClick={handleBarrierClick}>
+          <div className="modal-box" onClick={e => handleModalBox(e)}>
+            {children}
+          </div>
         </div>
       )}
     </Fragment>
   );
+
+  function handleBarrierClick() {
+    if (barrierDismiss !== false) {
+      modalState.setModalOpen(false);
+    }
+  }
+
+  function handleModalBox(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 };
 
 Modal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired
+  modalState: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
+  barrierDismiss: PropTypes.bool,
+  shouldCloseOnEsc: PropTypes.bool
+};
+
+Modal.defaultProps = {
+  barrierDismiss: true,
+  shouldCloseOnEsc: true
 };
 
 export default Modal;
