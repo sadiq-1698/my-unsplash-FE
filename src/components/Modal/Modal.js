f4d35df8643bd263bfbedc337988d47593aaf94b
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-13 20:52:23 +0530

    Defined modal component and added styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,22 @@
-import "./styles.js";
+import { Fragment } from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ children, isOpen }) => {
+  return (
+    <Fragment>
+      {isOpen && (
+        <div className="modal-backdrop">
+          <div className="modal-box">{children}</div>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default Modal;
