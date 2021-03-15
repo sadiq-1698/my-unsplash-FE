66a4157575b13d5cc94ae4fd90bcb661ec36217c
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-15 20:39:28 +0530

    Added Navbar elements

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,1 +1,21 @@
-import './styles.css';
+import "./styles.css";
+import Button from "../Button/Button";
+import InputField from "../InputField/InputField";
+
+const Navbar = () => {
+  return (
+    <div className="d-flex justify-space-between">
+      <div className="d-flex">
+        <img
+          className="nav-logo"
+          src={process.env.PUBLIC_URL + "/my_unsplash_logo.svg"}
+          alt="logo"
+        />
+        <InputField iconPrefix />
+      </div>
+      <Button color="green" text="Add a photo" />
+    </div>
+  );
+};
+
+export default Navbar;
