e04fc84428d98860cbe09cb24a73dfe7fd3c133e
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-15 20:40:32 +0530

    Included Navbar component

--- src/components/MyUnsplashApp/MyUnsplashApp.js
+++ src/components/MyUnsplashApp/MyUnsplashApp.js
@@ -2,18 +2,20 @@ import { Fragment } from "react";
 
 import Modal from "../Modal/Modal";
 import Button from "../Button/Button";
+import InputField from "../InputField/InputField";
+import AddGallery from "../Forms/AddGallery";
+import Navbar from "../Navbar/Navbar";
 
 import useModal from "../../hooks/useModal";
 
 import "./styles.css";
-import InputField from "../InputField/InputField";
-import AddGallery from "../Forms/AddGallery";
 
 const MyUnsplashApp = () => {
   const { toggleModal, closeModal, modalState } = useModal();
 
   return (
     <Fragment>
+      <Navbar />
       <Button color="green" text="Add image" onClick={toggleModal} />
 
       <InputField iconPrefix />
