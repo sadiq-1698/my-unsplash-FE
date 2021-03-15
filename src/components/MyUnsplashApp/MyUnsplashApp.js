5d1c11676564284a281d1bc2e92d79087a1968c2
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-15 20:47:42 +0530

    Removed unused component

--- src/components/MyUnsplashApp/MyUnsplashApp.js
+++ src/components/MyUnsplashApp/MyUnsplashApp.js
@@ -1,28 +1,14 @@
-import { Fragment } from "react";
-
-import Modal from "../Modal/Modal";
-import Button from "../Button/Button";
-import InputField from "../InputField/InputField";
-import AddGallery from "../Forms/AddGallery";
 import Navbar from "../Navbar/Navbar";
-
-import useModal from "../../hooks/useModal";
 
 import "./styles.css";
 
 const MyUnsplashApp = () => {
-  const { toggleModal, closeModal, modalState } = useModal();
-
   return (
-    <Fragment>
-      <Navbar />
-      <Button color="green" text="Add image" onClick={toggleModal} />
-
-      <InputField iconPrefix />
-      <Modal modalState={modalState}>
-        <AddGallery closeModal={closeModal} />
-      </Modal>
-    </Fragment>
+    <div className="app-wrapper">
+      <div className="nav-container">
+        <Navbar />
+      </div>
+    </div>
   );
 };
 
