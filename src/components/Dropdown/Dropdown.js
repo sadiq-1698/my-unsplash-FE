be9f6ca4bf36f96c55b7e49caa0e82f09a493517
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-17 09:17:04 +0530

    updated dropdown item styles

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -9,27 +9,32 @@ const Dropdown = () => {
   const items = ["Sadiq", "Umma", "Bapa", "Didi", "Bhaiyya"];
 
   return (
-    <div className="dropdown-container" onClick={toggleDropdown}>
-      <p className="link">
-        Select a gallery&nbsp;&nbsp;&nbsp;
-        <span>
-          <i
-            className={CHEVRON_DOWN + ` smooth ${galleries ? " rotate" : ""}`}
-            aria-hidden="true"
-          ></i>
-        </span>
-      </p>
-      {galleries ? (
-        <div className="dropdown-list">
-          {items.map(val => {
-            return (
-              <div key={val} className="dropdown-item">
-                {val}
-              </div>
-            );
-          })}
-        </div>
-      ) : null}
+    <div className="d-flex justify-space-between align-items-center">
+      <div>
+        <h1 className="current-gallery">Welcome</h1>
+      </div>
+      <div className="dropdown-container" onClick={toggleDropdown}>
+        <p className="link">
+          Select a gallery&nbsp;&nbsp;&nbsp;
+          <span>
+            <i
+              className={CHEVRON_DOWN + ` smooth ${galleries ? " rotate" : ""}`}
+              aria-hidden="true"
+            ></i>
+          </span>
+        </p>
+        {galleries ? (
+          <div className="dropdown-list">
+            {items.map(val => {
+              return (
+                <div key={val} className="dropdown-item">
+                  {val}
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
