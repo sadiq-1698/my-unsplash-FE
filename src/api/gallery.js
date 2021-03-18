9ce8069c5fbbdfdf2f192ad0431bdf9fd94abf0b
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-18 07:32:01 +0530

    added api call for adding a new gallery

--- src/api/gallery.js
+++ src/api/gallery.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+import { API_URL } from "../globals/constants";
+
+const addGallery = async (name, isPrivate, password) => {
+  const gallery = {
+    name: name,
+    isPrivate: isPrivate,
+    password: password
+  };
+  const response = await axios.post(API_URL, gallery);
+  return response;
+};
+
+export { addGallery, getAllGalleries };
