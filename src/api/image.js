bd40403ce6332f2feeb90852fa9db424586b1be5
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-19 07:23:01 +0530

    added image adding API endpoint

--- src/api/image.js
+++ src/api/image.js
@@ -1,1 +1,20 @@
 import axios from "axios";
+import { API_URL } from "../globals/constants";
+
+const addImage = async (label, url, galleryId) => {
+  const image = {
+    label: label,
+    url: url,
+    galleryId: galleryId
+  };
+  const response = await axios.post(API_URL + "image/add", image);
+  return response;
+};
+
+// const getAllImages = async () => {
+//   const response = await axios.get(API_URL + "image/all");
+//   const { data } = response;
+//   return data;
+// };
+
+// export { addImage, getAllImages };
