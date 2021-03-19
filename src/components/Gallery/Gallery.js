47d484605ab22a22907776e3682aae9ff4942a48
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-19 07:27:40 +0530

    structured image data in gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,11 +1,25 @@
+import PropTypes from "prop-types";
 import "./styles.css";
 
-const Gallery = () => {
+const Gallery = ({ images }) => {
   return (
     <div>
-      <div>Gallery</div>
+      <div>
+        {images.map(({ _id, url, label }) => {
+          return (
+            <div key={_id}>
+              <img src={url} alt={label} />
+              <p>{label}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
+};
+
+Gallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default Gallery;
