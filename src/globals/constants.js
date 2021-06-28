fd3369dc51e6f1170bacd00ba6014fe1a03678c8
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-06-28 23:21:14 +0530

    updating global variables

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,4 +1,4 @@
 export const IMAGE_UPLOADER_LINK =
   "https://sadiq-1698-image-uploader.netlify.app";
 
-export const API_URL = "https://sadiq-1698-my-unsplash.herokuapp.com";
+export const API_URL = "https://sadiq-1698-my-unsplash.herokuapp.com/";
